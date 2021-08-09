c5b8a6ffd8c92d4f9a18fce106a8f3070635c0b0
Vasant Tholappa <vasant.tholappa@gmail.com>
2021-08-09 22:24:27 +0530

    Everything except resizing to a new SQUARE grid of nxn user given dimensions

---
+++ script.js
@@ -0,0 +1,64 @@
+
+const container = document.querySelector(".container");
+
+let allElements;
+
+function grid_type(n)
+{
+    let i=0;
+    // console.log(n);
+    while(i<n*n)
+    {
+        const oneElement = document.createElement('div');
+        oneElement.setAttribute('class', 'gridElement'); 
+        oneElement.setAttribute('id', "div" + i); 
+        container.appendChild(oneElement);
+        i++;
+    }
+    allElements = document.querySelectorAll(".gridElement");
+    allElements.forEach(element => {
+        element.addEventListener("mouseover", () => {
+    
+            element.style.backgroundColor = 'blue';
+        });
+    });
+}
+
+
+grid_type(16);
+
+
+
+// const allElements = document.querySelectorAll(".gridElement");
+// allElements.forEach(element => {
+//     element.addEventListener("mouseover", () => {
+
+//         element.style.backgroundColor = 'blue';
+//     });
+// });
+
+const change = document.querySelector(".change");
+
+const btn = document.createElement('button');
+btn.setAttribute('id', "btn1"); 
+btn.textContent = 'Dimension Changer'; 
+change.appendChild(btn);
+
+
+function rem(D)
+{
+    
+    allElements.forEach(element => {
+        element.remove();
+    });
+
+    grid_type(D);
+}
+
+btn.addEventListener("click", () => {
+
+    let D = prompt("Enter D for DxD Dimension Grid");
+    if(!(D === "") && D<=100)
+    rem(D);
+    
+});
